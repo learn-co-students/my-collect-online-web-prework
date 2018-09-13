7a7fb92fb567ed8ecb15f58f9efc11a29b202f2f
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-09-13 11:26:07 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  new_collection = []
+  if array.length == 0
+    false
+  end
+  index = 0
+  while index< array.length
+  new_collection.push yield(array[index])
+    index = index + 1
+  
+  end
+  new_collection
+end
