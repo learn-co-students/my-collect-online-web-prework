be2b4f04acbbef23882a92662eed14347d3651c7
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 17:57:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  if !array.empty?
+    newArray = []
+    i = 0 
+    while i < array.length 
+    newArray << yield(array[i])
+    i += 1
+    end
+  else 
+    #empty array
+  end
+  return newArray
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+newArray = my_collect(collection) do |lang|
+  lang.upcase
+end
 
+puts newArray
