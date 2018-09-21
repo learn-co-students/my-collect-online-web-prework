e548e212ce6723835dd9b477acfe9d88b7b92e6a
Sharon A <sharon.alajajian@gmail.com>
2018-09-21 11:58:31 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(arr)
+    i = 0
+    newArr = []
+    while i < arr.length
+        #yield arr[i]  ##this is what we used to recreate the each method.
+        newArr << yield(arr[i])  ##do stuff to element in array and push it into new array
+        i += 1
+    end
+    ##arr
+    newArr
+end
