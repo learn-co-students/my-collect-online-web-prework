33739be7a43707460d142431f7b749d9a5165207
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 14:04:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(collection)
+  result = []
+  i = 0
+  while i < collection.length
+    result.push(yield(collection[i])) 
+    
+    #while the array is empty and the collection has items, push those items into the new collection
+    
+    i += 1
+  end
+  return result
+end
