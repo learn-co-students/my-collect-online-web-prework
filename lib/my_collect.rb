4017db97b96ed84c1a2c338236c5b89a0a4ec641
TruePieLover <41270795+TruePieLover@users.noreply.github.com>
2018-10-08 14:28:58 -0400

    Revert "Finished"

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,2 @@
-def my_collect(collection)
-  counter = 0
-  firstnamearray = []
-  while counter < collection.length
-    newyield = yield collection[counter]
-    firstnamearray.push(newyield)
-    counter += 1
-  end
-  firstnamearray
-end
-
 
 
