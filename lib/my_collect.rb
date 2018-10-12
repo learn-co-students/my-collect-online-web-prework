98a34e04bc6a119bc0057f661857c0fd24c31f4b
Linda Chang <lindachang186@gmail.com>
2018-10-12 02:56:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(collection)
+  i = 0
+  new = []
+  while i < collection.size
+    new << yield(collection[i])
+    i += 1
+  end
+  return new
+end
 
-
+#def my_collect(students)
+  #students.collect do |student|
+  #student.split(" ").first
+#end
+#end
