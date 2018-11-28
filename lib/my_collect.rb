377210926afd58cbae80b215087c298e6ffe452e
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-28 02:47:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(collection)
+   i = 0
+   new_col = []
+      while i < collection.length
+        new = yield collection[i]
+        new_col << new
+        i+= 1
+     end
+   
+   new_col
+ 
+end
 
