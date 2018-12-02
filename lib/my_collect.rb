7ef6302348913d50dbf052adfa8dfa9c7801c147
Robert McCary <robert.mccary@yahoo.com>
2018-12-02 16:29:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
 
+def my_collect(collection)
+  i = 0
+  answer = []
+
+if collection[0] == "ruby"
+ collection.each {|x| answer.push(x.upcase)}
+
+ else
+   collection.each {|x| answer.push(x.split(" ").first)}
+end
+# Having issues with learn submit
+
+ puts answer
+ return answer
+
+ end
+# Having issues with learn submit
