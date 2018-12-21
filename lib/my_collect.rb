dfeb1083b13329e426d9f89a001c471d9bb076b7
s greenbaum <gbs4ever2@gmail.com>
2018-12-21 20:55:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,14 +2,12 @@ def my_collect(words)
   i =0
 list= []
 while  i< words.length
-    yield words[i]
-  list << words
-
-end
+    list <<  yield words[i]
 
     i =i+1
 end
-
+return  list
+end
 
 
 #my_collect(words) do |lang|
