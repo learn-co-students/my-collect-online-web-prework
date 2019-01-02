eade578fdb064cac6044a160fe838544f9dc0235
Dylan Clements <dclements@protonmail.com>
2019-01-02 18:56:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   if block_given?
-    i = 0
+    i=0
     new_array = []
     while i < array.length
       new_array << yield(array[i])
-      i = i + 1
+      i+=1
     end
     new_array
   else
