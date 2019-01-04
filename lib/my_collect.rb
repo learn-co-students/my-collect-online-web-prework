7297308760ec6566fe4d4a7915ab041efb3cabde
A Glazer <avigayilglazer@gmail.com>
2019-01-04 10:40:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+lang = ['ruby', 'javascript', 'python', 'objective-c']
+
+def my_collect(lang)
+  lang = []
+  if lang.length == 0
+    return []
+  else
+    yield lang
+  end
+  lang
+end
 
 
+my_collect(lang) do |x|
+  x.upcase
+end
