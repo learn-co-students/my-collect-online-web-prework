ad536f034a5b02c0c493c0b95939b41ecdef3102
A Glazer <avigayilglazer@gmail.com>
2019-01-05 21:02:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,20 @@
 lang = ['ruby', 'javascript', 'python', 'objective-c']
+beep = [1, 2, 3]
+hat = ["beekkkkk"]
 
-def my_collect(lang)
-  lang = []
-  while lang.length > 1
-    lang.push yield lang[x]
+def my_collect(lang2)
+  x = 0
+
+  while x < lang2.size
+    yield lang2[x]
+
+    x += 1
   end
-  lang
+
+  return lang2
+
 end
 
-
-my_collect(lang) do |x|
-  "#{x}".upcase
-
+my_collect(lang) do |word|
+  word.upcase
 end
