9b59b5cd49ab54f2c9c8dae03addcde9aa9ea7b8
Mark Kanko <mkanko66@gmail.com>
2019-01-26 00:29:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(list)
+  
+  modified_list = []
+  
+  item = 0 
+  
+  while item < list.length 
+  
+    modified_list << yield(list[item]) 
+   
+    item = item + 1 
+   
+  end 
+  
+  modified_list
+    
+end  
 
 
