342ded8c600500cd5a16b96ff702d0666c58b40b
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-29 21:59:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  while i < array.length
+    yield array.join(",").upcase.split(",")
+    i += 1
+  end
 
+end
 
+# array = array.join(", ")
+# array = array.upcase
+# array = array.split(",").first
