067f61c2b12bee8b0febec5a258e8272c965ecf6
Kyle Spencer <spencerk2@vcu.edu>
2019-01-31 03:11:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
 
+collected_array = []
+  
+  i = 0
+  while i < array.length
+    c = yield array[i]
+    collected_array << c
+    i = i + 1
+  end
+  collected_array
+
+end
 
