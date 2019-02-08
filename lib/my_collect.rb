1a29e2624deb95f497860eead4e6b017973f6023
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-08 19:51:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  outputArr = []
+  i = 0 
+  while i < array.length 
+    yield array[i]
+    outputArr.push(yield array[i])
+    i = i + 1 
+  end
+  outputArr
+end 
 
-
+#def my_each(array)
+  # code here
+#  i = 0 
+#  while i < array.length 
+#    yield array[i] 
+#    i = i + 1 
+#  end
+#  array 
+#end
