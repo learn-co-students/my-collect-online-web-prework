9c824c3d32e682115409866831ca12961db20cbc
Katie Blizzard <kblizzard11@gmail.com>
2019-02-09 18:26:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0 
+    new_array = []
+    while i < array.length 
+      new_array << yield(array[i])
+      i += 1 
+      
+    end
+      return new_array
+  else
+    "No block given"
+  end
+end
 
