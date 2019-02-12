5ec9f09614076f1541b0616efd3a9e4545a3ee1c
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-12 16:20:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = Array.new
+    
+    while i < array.length
+      new_array << yield(array[i])
+      i += 1
+    end
+    new_array
+  else
+    "Block not provided"
+  end
+end
+
+my_collect(names) do |name|
+  name.split(" ").first
+end
 
