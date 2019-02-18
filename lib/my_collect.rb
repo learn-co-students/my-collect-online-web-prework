ccecefd91670ae99e6d3e96a1d34d8344208e864
Ryan Pethel <arpethel@gmail.com>
2019-02-18 23:11:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(array)
+  i = 0
 
+  my_arr = []
 
+  while i < array.length
+    block_given?
+    my_arr << yield(array[i])
+    i += 1
+  end
+  my_arr
+end
+
+# students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+#
+# my_collect(students) do |student|
+#   student.split(" ").first
+# end
+
+# languages = ["ruby", "javascript", "python", "objective-c"]
+#
+# my_collect(languages) do |language|
+#   language.upcase
+# end
