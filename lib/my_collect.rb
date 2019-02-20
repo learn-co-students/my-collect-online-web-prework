f265035460230b5018f6ca59e986a4c4089e936e
Darren L <darren.lou@gmail.com>
2019-02-20 23:19:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,14 +2,13 @@ def my_collect(collection)
   i = 0
   array = []
   while (i < collection.length) do
-    yield collection[i]
-    
     if collection[i].include?(" ")
+      yield collection[i].split(" ").first
       array << collection[i].split(" ").first
     else
+      yield collection[i].upcase
       array << collection[i].upcase
     end
-    
     i += 1
   end
   return array
