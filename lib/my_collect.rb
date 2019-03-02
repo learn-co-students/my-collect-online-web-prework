5c513dc01496e1c088c42ec40cdc8a82d726000a
Daniel Schneider <daschne8@gmail.com>
2019-03-02 02:14:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << array[i]
+    item = yield array[i]
+    new_array << item
+    i += 1
   end
   new_array
 end
