fd62f810385fa625836ab80fdb5fd3a70a086e16
Alison Golden <alisong779@yahoo.com>
2019-04-15 02:36:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
+  new_var = []
   i = 0 
   while i < array.length 
-  yield(array[i])
+  new_var << yield(array[i])
     i = i + 1 
-  end 
-  array  
 end
+  new_var
 end
 
