b3d6d97e4543fea920230f648deda6586bf734e6
Jamie Vullo <jvullo@protonmail.com>
2019-04-23 14:41:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+  new_array = []
+  
+  i = 0 
+  while i < array.length 
+  #  x = array[i].split(" ").first
+    new_array << yield(array[i]) 
+    
+    
+    i += 1 
+  end
+  
+  return new_array
+end
 
