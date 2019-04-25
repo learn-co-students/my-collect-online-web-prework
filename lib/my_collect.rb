98e9fe5a22b6e5df030d45ea384635eae33198b7
Jess Rezac <jess.rezac@gmail.com>
2019-04-25 18:16:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
 
+  new_collect = []
 
+  while i < collection.length
+    changed_i = yield(collection[i])
+    new_collect << changed_i
+    i += 1
+  end
+
+  new_collect
+end
