c7f53a722c4bc5f6fdb507e0d59ebad435bbf479
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 13:51:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  i = 0
+  newArray = []
+  while i < array.count do
+    newArray.push(yield array[i])
+    i += 1
+  end
+  newArray
+end
 
-
+my_collect(["ruby", "javascript","python","objective-c"]) {|lang| lang.upcase}
