b8403bb69a318535ab14fe1d9119bce1b1374cf4
Brian Salemi <briandsalemi@gmail.com>
2019-04-27 15:40:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+    ##block_given?
+    new_collection = []
+    i = 0
+    while i < collection.length
+      new_collection << yield collection[i]
+      i = i + 1
+    end
+    new_collection
+end
