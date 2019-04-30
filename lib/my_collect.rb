32e96bda6ca9c0d27e4dbf234bde7e400cb35f48
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-30 11:30:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
-def my_collection(array)
+def my_collect(array)
   i = 0
-  new_array = []
+  modified_array = []
   while i < array.length
-  new.array << yield(array[i])
+  modified_array << yield(array[i])
   i+=1
 end
-new.array
+modified_array
 end
+
