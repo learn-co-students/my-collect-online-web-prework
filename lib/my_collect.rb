a22356a5e3cf090252f15b227020d9835755af26
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-14 23:13:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
+def my_collect(empty)
+  if empty == []
+    return empty
+  elsif empty[0].include?(" ")
+    empty.collect do |empty|
+      empty.capitalize
+      gloob = empty.split
+      gloob.first
+    end
+    else 
+  empty.collect do |empty|
+    empty.upcase
+  end
+  end
+end
 
