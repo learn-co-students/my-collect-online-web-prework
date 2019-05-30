3b8ef620818b1b77b06df1f17d1ab4fdfd4db3aa
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 21:28:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(words)
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < words.length
+      yielded_value = yield words[i]
+      new_collection << yielded_value
+      i = i + 1
+    end
+  end
+  new_collection
+end
