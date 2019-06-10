9ff96da0618e39fb3246f72813434d9104923eda
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 03:01:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
+def my_collect(empty_array)
+  new_array = Array.new
 
+  if block_given?
+    i = 0 
+    while i < empty_array.length 
+      new_array << yield(empty_array[i])
+      i += 1
+    end
+  else
+    nil
+  end
+  new_array
+end
